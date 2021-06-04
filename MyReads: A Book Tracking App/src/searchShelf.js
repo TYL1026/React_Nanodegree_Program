7172e966c0177bc89ad0a244324364d841a880f3
TonyL <65515982+TYL1026@users.noreply.github.com>
2021-06-04 12:43:23 -0700

    Update searchShelf.js

--- MyReads: A Book Tracking App/src/searchShelf.js
+++ MyReads: A Book Tracking App/src/searchShelf.js
@@ -17,7 +17,13 @@ class searchShelf extends Component{
                                 <li key={book.id}>
                                     <div className="book">
                                         <div className="book-top">
-                                            <div className="book-cover" style={{width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}></div>
+                                            {
+                                                book.hasOwnProperty('smallThumbnail')?(
+                                                    <div className="book-cover" style={{width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}></div>
+                                                ):(
+                                                    <div className="book-cover" style={{width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail })`}}></div>
+                                                )
+                                            }
                                                 <div className="book-shelf-changer">
                                                 <select onChange={(event)=>this.handleShelfChange(event,book.id)}>
                                                     <option value="move" disabled>Move to...</option>
